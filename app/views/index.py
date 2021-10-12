db992d2bdadb8ce1e06d21cdf8b5a1c17ee22a88
Sean McGoff <srobertmcgoff@gmail.com>
2021-10-11 20:55:12 -0400

    Added template and static folder structure

--- app/views/index.py
+++ app/views/index.py
@@ -1,7 +1,8 @@
+from flask import render_template
 from app import app
 
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html", name="Coding Club")
