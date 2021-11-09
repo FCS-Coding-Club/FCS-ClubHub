774378b7b0b2442ee42439e4fab559b25c79206b
Sean McGoff <seentheshadow@gmail.com>
2021-11-09 17:52:00 -0500

    Fingers Crossed testing works now

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 import os
-from app import app
+from app import create_app
 # Debug Checking
 if os.environ.get('DEBUG') is None:
     os.environ["DEBUG"] = "True"
-# TODO: Unit Testing?
+if os.environ.get('TESTING') is None:
+    os.environ["TESTING"] = "False"
 
-# Run App
-app.run(host="0.0.0.0", port=3000, debug=os.environ["DEBUG"])
+if os.environ.get('TESTING') == "False":
+    app = create_app()
+    app.run(host="0.0.0.0", port=3000, debug=os.environ["DEBUG"])
+else:
+    print("TESTING flag set to not False, cannot run.")
