bc3b02ab9df95a31dc250bf3e7404a353deb128b
Sean McGoff <seentheshadow@gmail.com>
2021-11-21 16:53:59 -0500

    Registration, Login, and Bootstrap

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import os
+import random
+import string
 from app import create_app
 # Debug Checking
 if os.environ.get('DEBUG') is None:
@@ -8,6 +10,8 @@ if os.environ.get('TESTING') is None:
 
 if os.environ.get('TESTING') == "False":
     app = create_app()
+    os.environ["SECRET_KEY"] = "".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(12))
+    os.environ["WTF_CSRF_SECRET_KEY"] = "".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(12))
     app.run(host="0.0.0.0", port=3000, debug=os.environ["DEBUG"])
 else:
     print("TESTING flag set to not False, cannot run.")
--- tests/test_flask.py
+++ tests/test_flask.py
@@ -10,6 +10,11 @@ def client():
         yield client
 
 
-def test_basic_request(client):
+def test_home_page_request(client):
     res = client.get("/")
+    assert res.status_code == 200
+
+
+def test_register_page_request(client):
+    res = client.get("/register")
     assert res.status_code == 200
